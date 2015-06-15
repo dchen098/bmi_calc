73d4f13e37dde9bf6e640cdbe0e28fefe837be16
Alex Shi <yoip1117@gmail.com>
2015-06-15 18:14:30 -0500

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -4,28 +4,27 @@ print("\t\t\tMade by Daniel Chen")
 print(" ")
 print(" ")
 
-
+#daniel ur so stupid
 
 weight = input("What is your weight in pounds?  ")
 height = input("What is your height in inches?  ")
 bmi = (weight * 703) / (height * height)
-bmi1 = str(bmi)
-print ("Your BMI is " +bmi1)
+print ("Your BMI is " + str(bmi))
 
-if bmi1 < 18.5:
-	type = str("underweight")
-if 18.5 <= bmi1 > 25:
-	type = str("normal")
-if 25 <= bmi1 > 30:
-	type = str("overweight")
-if bmi1 < 30:
-	type = ("obese")
+if bmi < 18.5:
+	type = "underweight"
+if 18.5 <= bmi < 25:
+	type = "normal"
+if 25 <= bmi < 30:
+	type = "overweight"
+if bmi < 30:
+	type = "obese"
 	
 print("You are " + type)
 
-if type == obese or type == overweight
+if type == "obese" or type == "overweight":
 	print("Try losing some weight")
-if type == underweight
+if type == "underweight":
 	print("Try gaining some weight")
-if type == normal
+if type == "normal":
 	print("You are good")
